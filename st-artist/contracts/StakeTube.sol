ca3b206c1a02c38567fe5db23d559b14f5d6837b
arkeo01 <kishor.1@iitj.ac.in>
2021-01-10 16:12:54 +0530

    AddVideo and Tip implemented with mapping idToVideo instead of Videos Array

--- st-artist/contracts/StakeTube.sol
+++ st-artist/contracts/StakeTube.sol
@@ -19,11 +19,12 @@ contract StakeTube {
     /// @notice total number of videos stored currently.
     uint public totalVideos;
 
-    /// @notice Array containing all the videos
-    Video[] public videos;
+    /// @notice Mapping from video id to the video struct stored on blockchain
+    mapping (uint => Video) public idToVideo;
 
-    /// @notice mapping from id to Video object
-    mapping(uint => Video) public idToVideo;
+    /// @notice This can also be an alternative structure in which the videos can be stored
+    /// @notice See for Tradeoffs
+    // Video[] public videos;
 
     /// @notice Event emitted after video is addVideo
     event videoAdded(
@@ -33,31 +34,55 @@ contract StakeTube {
 
     /// @notice Event after the amount is tipped
     event tipped(
-        uint videoId
+        uint videoId,
+        address payable creator,
+        address payable tipper
     );
 
     /// @dev Adds Video Details to the blockchain
     /// @dev Add the necessary require statements
     /// @param _name Name of Video
     /// @param _description Description of Video
+    // function addVideo(string memory _name, string memory _description) public {
+    //     totalVideos++;
+    //     uint _videoId = totalVideos;
+    //     videos.push(Video(
+    //         _videoId,
+    //         _name,
+    //         _description,
+    //         msg.sender
+    //     ));
+    //     emit videoAdded(_videoId, msg.sender);
+    // }
     function addVideo(string memory _name, string memory _description) public {
         totalVideos++;
         uint _videoId = totalVideos;
-        videos.push(Video(
+        Video memory video = Video(
             _videoId,
             _name,
             _description,
             msg.sender
-        ));
+        );
+        idToVideo[_videoId] = video;
         emit videoAdded(_videoId, msg.sender);
+
     }
 
     /// @notice Function for tipping the creator of the video
+    /// @param _videoId Id of the video
     /// @dev CAUTION Currently it is taking the index of array, to be modified to take video Id
-    function tip() public {
-        Video storage myVideo = videos[0];
+    function tip(uint _videoId) public payable{
+        Video storage myVideo = idToVideo[_videoId];
+        address payable creator = myVideo.creator;
         uint myVideoId = myVideo.videoId;
-        emit tipped(myVideoId);
+
+        require(
+            msg.value > 0, 
+            "The tip should be non-zero."
+        );
+
+        creator.transfer(msg.value);
+        emit tipped(myVideoId, creator, msg.sender);
     }
 
 }
